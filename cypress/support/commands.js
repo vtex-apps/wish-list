ea112db97ea5791235f17061a441d0c152920e0d
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-05-16 13:18:11 +0530

    Include vtex.bitcot-qa-theme in removeApps + fix slowness of loading storefront issue

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,17 @@
 import selectors from './common/selectors'
 import wishListSelectors from './wish-list-selectors'
+
 const wishlistJson = '.wishlist.json'
 
 Cypress.Commands.add('openStoreFront', (login = false) => {
   cy.intercept('**/rc.vtex.com.br/api/events').as('events')
   cy.visit('/')
   cy.wait('@events')
+  // Page loads heart icon only on scroll
+  // So, scroll first then look for selectors
+  cy.scrollTo(0, 800)
+  cy.wait(1000)
+  cy.scrollTo(0, -200)
   if (login === true) {
     cy.get(selectors.ProfileLabel, { timeout: 20000 })
       .should('be.visible')
